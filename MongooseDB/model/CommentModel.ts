d1140b0ec89c896c281a2c1a5340293597d37ce3
saifCapstone <ssheikh@seattleu.edu>
2021-05-03 22:37:39 -0700

    I forgot to switch branches, woops my b

--- MongooseDB/model/CommentModel.ts
+++ MongooseDB/model/CommentModel.ts
@@ -1,6 +1,6 @@
 import Mongoose = require("mongoose");
-import {DataAccess} from './../DataAccess';
-import {ICommentModel} from '../interfaces/ICommentModel';
+import { DataAccess } from "./../DataAccess";
+import { ICommentModel } from "../interfaces/ICommentModel";
 import { STATUS_CODES } from "http";
 
 let mongooseConnection = DataAccess.mongooseConnection;
@@ -16,59 +16,70 @@ delete: delete a comment
 */
 
 class CommentModel {
-    public schema:any;
-    public innerSchema:any;
-    public model:any;
+  public schema: any;
+  public innerSchema: any;
+  public model: any;
 
-    public constructor() {
-        this.createSchema();
-        this.createModel();
-    }
+  public constructor() {
+    this.createSchema();
+    this.createModel();
+  }
 
-    public createSchema(): void {
-        this.schema = new Mongoose.Schema(
-            {
-                postId: String,
-                userId: String,
-                commentId: String,
-                content: String,
-                likes: Number,
-                timestamp: Date
-            }, {collection: 'comments'}
-        );
-    }
+  public createSchema(): void {
+    this.schema = new Mongoose.Schema(
+      {
+        postId: String,
+        userId: String,
+        commentId: String,
+        content: String,
+        likes: Number,
+        timestamp: Date,
+      },
+      { collection: "comments" }
+    );
+  }
 
-    public createModel(): void {
-        this.model = mongooseConnection.model<ICommentModel>("Comments", this.schema);
-    }
-    
-    // view a comment 
-    public retrieveComment(response:any, filter:Object) {
-        var query = this.model.findOne(filter);
-        query.exec( (err, comment) => {
-            if (err) {
-                console.log('Error retrieving comment.'); 
-            }
-            response.json(comment);
-        });
-    }
+  public createModel(): void {
+    this.model = mongooseConnection.model<ICommentModel>(
+      "Comments",
+      this.schema
+    );
+  }
 
-    // delete a comment 
-    public deleteComment(response: any, filter: Object) {
-        var query = this.model.deleteOne(filter); 
-        query.exec( (err, comment) => {
-            if (err) {
-                console.log('Error deleting comment.'); 
-            }
-        }); 
-    }
+  // view a comment
+  public retrieveComment(response: any, filter: Object) {
+    var query = this.model.findOne(filter);
+    query.exec((err, comment) => {
+      if (err) {
+        console.log("Error retrieving comment.");
+      }
+      response.json(comment);
+    });
+  }
 
-    // like a comment
-    public likeComment(response: any, filter: Object) {
-        var query = this.model.findOne(Object); 
-        query.likes += 1;
-        query.save(); 
-    }
+  // delete a comment
+  public deleteComment(response: any, filter: Object) {
+    var query = this.model.deleteOne(filter);
+    query.exec((err, comment) => {
+      if (err) {
+        console.log("Error deleting comment.");
+      }
+    });
+  }
 
+  // like a comment
+  public likeComment(response: any, filter: Object) {
+    var query = this.model.findOne(Object);
+    query.likes += 1;
+    query.save();
+  }
+
+  // get all comments (via post id)
+  public getAllComments(response: any, filter: Object) {
+    var query = this.model.find(filter);
+    query.exec((err, post) => {
+      response.json(post);
+    });
+  }
 }
-export {CommentModel};
+export { CommentModel };
