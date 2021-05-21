291ed93f4b2d7e3422e41e724d96017c39509fc0
Laura Larson <31149925+laurakelarson@users.noreply.github.com>
2021-05-20 23:06:19 -0700

    Added new components for feed and meme list item

---
+++ angular/src/app/meme-list-item/meme-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meme-list-item',
+  templateUrl: './meme-list-item.component.html',
+  styleUrls: ['./meme-list-item.component.css']
+})
+export class MemeListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
