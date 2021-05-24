f62f26bd40d0c48375648b86734b5cb533eeef46
saifCapstone <ssheikh@seattleu.edu>
2021-05-24 09:24:18 -0700

    Deleted Hilerios extra files
    Last time he dinged a team for not deleting the extra files so I just got rid of most of them.

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -6,28 +6,20 @@ import { HttpModule } from "@angular/http";
 
 import { AppComponent } from "./app.component";
 import { WelcomeComponent } from "./welcome/welcome.component";
-import { ListsComponent } from "./lists/lists.component";
 import { APP_BASE_HREF } from "@angular/common";
 import { routing } from "./app.routing";
-import { ListComponent } from "./list/list.component";
-import { ListsService } from "app/list-service.service";
-import { ListsTableComponent } from "./lists/lists-table/lists-table.component";
-import { ItemsTableComponent } from "./list/items-table/items-table.component";
 
 //Added this
 import { MemeComponent } from "./meme/meme.component";
 import { MemeService } from "app/meme-service.service";
-import { FeedComponent } from './feed/feed.component';
-import { MemeListItemComponent } from './meme-list-item/meme-list-item.component';
+import { FeedComponent } from "./feed/feed.component";
+import { MemeListItemComponent } from "./meme-list-item/meme-list-item.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    ListsComponent,
-    ListComponent,
-    ListsTableComponent,
-    ItemsTableComponent,
+
     MemeComponent,
     FeedComponent,
     MemeListItemComponent,
@@ -35,11 +27,7 @@ import { MemeListItemComponent } from './meme-list-item/meme-list-item.component
   imports: [BrowserModule, FormsModule, HttpModule, routing],
 
   //Aded meme service to the end of this
-  providers: [
-    { provide: APP_BASE_HREF, useValue: "/" },
-    ListsService,
-    MemeService,
-  ],
+  providers: [{ provide: APP_BASE_HREF, useValue: "/" }, MemeService],
 
   bootstrap: [AppComponent],
 })
